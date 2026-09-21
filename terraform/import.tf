78186c78dd9be4c8e8da2e09965e5a0bdabd195d
lvo <luca.vonsaal@avm.swiss>
2026-09-21 12:59:10 +0200

    chore: sync terraform import.tf with the current cluster ID after rebuild

--- terraform/import.tf
+++ terraform/import.tf
@@ -2,6 +2,6 @@
 # statt ihn neu zu erstellen. Cluster-ID per "doctl kubernetes cluster get k8s-vscmodul" ermittelt.
 import {
   to = digitalocean_kubernetes_cluster.this
-  # Cluster wurde am 2026-09-20 ab- und wieder aufgebaut (neue ID, siehe DECISIONS.md).
-  id = "0ffcdeea-474f-4583-a489-4a16cc2b5b56"
+  # Cluster wurde zuletzt am 2026-09-21 ab- und wieder aufgebaut (neue ID, siehe DECISIONS.md).
+  id = "a13e2e31-2a11-4bbf-bcdf-b05f1e72799d"
 }
